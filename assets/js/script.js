f1e0e09251aba8356b1d8e74587c169c6985aa11
Shalah <zoungranaala@gmail.com>
2022-01-22 02:30:00 -0500

    Created an html page and added few headings. Also added a click button for Scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("test")
+//console.log("test")
+// var body = document.body;
+
+// var centerContentEl = document.getElementsByClassName("centerContent")
+
+// centerContentEl.style.color = "blue";
+
+//Declare Variables
+var viewScoresEl = document.querySelector("#view-scores");
+
+
+viewScoresEl.addEventListener("click", function() {
+    console.log("It works");
+})
