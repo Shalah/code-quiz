508ea3432b5fb79814df63a3e095351c45e4d683
Shalah <zoungranaala@gmail.com>
2022-01-22 16:32:01 -0500

    Added a button to trigger the quiz.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,16 @@
 // centerContentEl.style.color = "blue";
 
 //Declare Variables
-var viewScoresEl = document.querySelector("#view-scores");
+//var viewScoresEl = document.querySelector("#view-scores");
 
+//Creating a var for the button start
+var startBtn = document.querySelector(".start");
 
-viewScoresEl.addEventListener("click", function() {
+/*viewScoresEl.addEventListener("click", function() {
     console.log("It works");
+}) */
+
+//Listening for the click of the button
+startBtn.addEventListener("click", function(){
+    console.log("This is a button");
 })
