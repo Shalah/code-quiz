356e7eb9c7a357ad7a5ae0839e1dbe6a71e9321a
Shalah <zoungranaala@gmail.com>
2022-01-22 21:24:20 -0500

    Added a function to clear content when a button is clicked. Not working yet.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,29 @@
-//console.log("test")
-// var body = document.body;
-
-// var centerContentEl = document.getElementsByClassName("centerContent")
-
-// centerContentEl.style.color = "blue";
-
 //Declare Variables
 //var viewScoresEl = document.querySelector("#view-scores");
 
 //Creating a var for the button start
 var startBtn = document.querySelector(".start");
+var divContent = document.getElementById("removeContentDiv");
 
 /*viewScoresEl.addEventListener("click", function() {
     console.log("It works");
 }) */
 
 //Listening for the click of the button
-startBtn.addEventListener("click", function(){
-    console.log("This is a button");
-})
+document.addEventListener("click", test)
+
+function test(){
+    document.body.style.backgroundColor = "red";
+    removeContentEl();
+}
+
+function removeContentEl(){
+    divContent.removeChild(divContent)
+}
+
+function myFunction() {
+    var list = document.getElementById("myList");
+    while (list.hasChildNodes()) {
+      list.removeChild(list.firstChild);
+    }
+  }
