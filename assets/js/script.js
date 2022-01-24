d1b7e0b07684cf246c6bf6ddf65c0088cdf39124
Shalah <zoungranaala@gmail.com>
2022-01-23 19:13:02 -0500

    Added one quiz question that only appears when the button start is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,39 @@
 //Declare Variables
 var startEl = document.querySelector("#start");  //Creating a var for the button start
 var timerEl = document.querySelector("#timer");   // creating a var for the timer
-
+var hideQuizEl = document.querySelector(".hide-quiz")  // create a var to hide quiz content
 var secondsLeft = 5; //This is the inital time users will start the game with
-
+// var removeContentEl = document.querySelectorall(".remove-content-div")  //This is to remove the content right after the button is clicked
 
 //Function to set the timer
 function setTimer(){
     var timerInterval = setInterval(function(){
         secondsLeft--;
         timerEl.textContent = "Time: " + secondsLeft;
-        
+       // removeContent()
+        displayQuiz();
         if (secondsLeft === 0) {
-            clearInterval(timerInterval);  // This stops the timer from going to negative values. It takes a parameter that was used for "setInterval"
+            clearInterval(timerInterval);  // Stops the timer from going to negative. Takes a parameter that was used for "setInterval"
             alert ("Still working on it")
         }
     }, 1000)
 }
+
+//Function to call the quiz
+function displayQuiz(){
+    hideQuizEl.style.display = "block"
+}
+
+//function to remove the content right after the button is clicked
+// function removeContent(){
+//     removeContentEl.remove();
+// }
 
 
 startEl.addEventListener("click", function(){        //Listening for the click of the button
     setTimer();
     //console.log("test")
 })
-
-/* 
-    which of the following tag is used to create a vertical line
-    <br>                       <p>
-    <vertical>                 <hr>
-*/
-    
-/* 
-    How can you add a comment in Javascript
-
-    // this is a comment //          // This is a comment
-
-    /* this is a comment //          // This is the comment /*
-
-*/ 
-
-/* 
-    The # symbol is meant to indicate that the selector is:
-
-    class                           tag
-
-    id                              function
-*/
-
-/* 
-    How do you display a hyperlink with no underline
-
-    a("no underline")                               a{decoration: no underline}
-
-    a{text-decoration: none}                        a{hyperlink: no underline}
-*/
-
-/* 
-    How do you inlcude an internal style sheet in HTML
-
-    <HTML>                                      <style>
-
-    <CSS>                                       <link>
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
