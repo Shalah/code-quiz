248ba240f184578fe8263ce777f46d008111e6bf
Shalah <zoungranaala@gmail.com>
2022-01-25 17:44:41 -0500

    Rewrote the most of the code to include a loop.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
 //Declare Variables
 var startEl = document.querySelector("#start");  //Creating a var for the button start
 var timerEl = document.querySelector("#timer");   // creating a var for the timer
-var secondsLeft = 20; //This is the inital time users will start the game with
 
-var hideQuizEl1 = document.querySelector(".hide-quiz-1")  // create a var to hide quiz content 1
-var hideQuizEl2 = document.querySelector(".hide-quiz-2")  // create a var to hide quiz content 2
-var hideQuizEl3 = document.querySelector(".hide-quiz-3")  // create a var to hide quiz content 3
-var hideQuizEl4 = document.querySelector(".hide-quiz-4")  // create a var to hide quiz content 4
-var hideQuizEl5 = document.querySelector(".hide-quiz-5")  // create a var to hide quiz content 2
+var secondsLeft = 5; //This is the inital time users will start the game with
 
 var removeContentEl = document.querySelector(".remove-content-div")  //This is to remove the content right after the button is clicked
-var rightAnswer1 = document.querySelector(".right-answer-1")   //Var for right answer 1
-var rightAnswer2 = document.querySelector(".right-answer-2")   //Var for right answer 2
-var rightAnswer3 = document.querySelector(".right-answer-3")   //Var for right answer 3
-var rightAnswer4 = document.querySelector(".right-answer-4")   //Var for right answer 4
-var rightAnswer5 = document.querySelector(".right-answer-5")   //Var for right answer 5
 
+var questionsEl = document.querySelectorAll('.quiz-questions'); //This is for everything with a class questions
+
+var hideQuizEl1 = document.querySelector(".hide-quiz")  // create a var to hide quiz content 
+
+var rightAnswer = document.querySelector(".right-answer")   //Var for right answer 
+
+var correctAnswerEl = document.querySelectorAll("#correct-answer")  //This is for the user to see when they select the right answer
+correctAnswerEl.textContent = "Correct Answer!!"
 
 
 //Function to set the timer
@@ -23,9 +21,13 @@ function setTimer(){
     var timerInterval = setInterval(function(){
         secondsLeft--;
         timerEl.textContent = "Time: " + secondsLeft;
-        removeContent()
-        displayQuiz1();
+        removeContentEl.remove();
+        //displayQuiz1();
         //UserAnswer();
+
+        loopingQuestions();
+        
+
         if (secondsLeft === 0) {
             clearInterval(timerInterval);  // Stops the timer from going to negative. Takes a parameter that was used for "setInterval"
             //alert ("Still working on it")
@@ -33,82 +35,28 @@ function setTimer(){
     }, 1000)  // 1000 is the time in millisecond (the function repeats itself every 1000 ms)
 }
 
-//Function to call the first quiz
-function displayQuiz1(){
-    hideQuizEl1.style.display = "block";
-}
-
-//Function to call the second quiz
-function displayQuiz2(){
-    hideQuizEl2.style.display = "block";
-} 
-
-//Function to call the third quiz
-function displayQuiz3(){
-    hideQuizEl3.style.display = "block";
-} 
-
-//Function to call the fourth quiz
-function displayQuiz4(){
-    hideQuizEl4.style.display = "block";
-} 
-
-//Function to call the fifth quiz
-function displayQuiz5(){
-    hideQuizEl5.style.display = "block";
-} 
 
 
-rightAnswer1.addEventListener("click", function(){
-    //console.log("Right answer")
-    hideQuizEl1.remove();
-    alert("Right Answer")
-    displayQuiz2();
-})
 
-rightAnswer2.addEventListener("click", function(){
-    //console.log("Right answer")
-    hideQuizEl2.remove();
-    alert("Right Answer")
-    displayQuiz3();
-})
+function loopingQuestions(){
+    for (let i=0; i < questionsEl.length; i++) {
+        //console.log (questionsEl[i])
 
-rightAnswer3.addEventListener("click", function(){
-    //console.log("Right answer")
-    hideQuizEl3.remove();
-    alert("Right Answer")
-    displayQuiz4();
-})
-
-rightAnswer4.addEventListener("click", function(){
-    //console.log("Right answer")
-    hideQuizEl4.remove();
-    alert("Right Answer")
-    displayQuiz5();
-})
-
-rightAnswer5.addEventListener("click", function(){
-    //console.log("Right answer")
-    hideQuizEl5.remove();
-    alert("Right Answer")
-})
-
-/*This is to display the user picked the right answer
-function UserAnswer(){
-    if (rightAnswer1 === "click"){
-        alert("Right Answer")
+        var begin = questionsEl[i];
+        //begin.style.display = 'block';
+        
     }
-    // else {
-    //     alert("Wrong answer")
-    // }
-} */
-
-
-//function to remove the content right after the button is clicked
-// I can simplify this by deleting the function and just call this "removeContentEl.remove();" 
-function removeContent(){
-    removeContentEl.remove();
+    
 }
+
+
+
+rightAnswer.addEventListener ('click', function(){
+    console.log('Right answer');
+    correctAnswerEl.style.display = 'block';
+    begin.style.display = 'none';
+    i++;
+})
 
 
 
@@ -134,11 +82,90 @@ startEl.addEventListener("click", function(){        //Listening for the click o
 
 
 
-// Not working
-//var startBtn = document.querySelector("#start");
-//startBtn.addEventListener("click", test)
 
-// function test(){
-//     console.log("lets see")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function to remove the content right after the button is clicked
+// I can simplify this by deleting the function and just call this "removeContentEl.remove();" 
+/*
+function removeContent(){
     
-// }
+}
+
+
+
+
+//Function to call the first quiz
+function displayQuiz1(){
+    hideQuizEl1.style.display = "block";
+}
+
+*/
+
+
+
+
+
+
+
+
+// rightAnswer1.addEventListener("click", function(){
+//     //console.log("Right answer")
+//     hideQuizEl1.remove();
+//     alert("Right Answer")
+//     displayQuiz2();
+// })
+
+
+/*This is to display the user picked the right answer
+function UserAnswer(){
+    if (rightAnswer1 === "click"){
+        alert("Right Answer")
+    }
+    // else {
+    //     alert("Wrong answer")
+    // }
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
