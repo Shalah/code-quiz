27cb0b78ea9ad1a9a2df85aa082ce6d0e4609b22
Shalah <zoungranaala@gmail.com>
2022-01-26 21:08:17 -0500

    Tried adding some loops and functions to make the page function correctly but still did not behave as it should have.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# code-quiz
+# Code-Quiz
+
+## Description
+This is a code quiz page. Basically, the user has to answer a set of questions and each right answer is 5 points. A wrong answer would deduct 10 points from the total. There is also a timer that increases and decreases depending on how good the quiz is going.
+ 
+## Deployment
+
+The application is displayed with GitHub pages [here](linkhere)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,13 @@
 
 //Declare Variables
 var body = document.body;
-var startEl = document.querySelector("#start");  //Creating a var for the button start
+var startEl = document.querySelector("#start");  // Creating a var for the button start
 var timerEl = document.querySelector("#timer");   // creating a var for the timer
-var secondsLeft = 20; //This is the inital time users will start the game with
-
+var submitEl = document.querySelector("#submit"); //var for the button submit
+var secondsLeft = 60; // This is the inital time users will start the game with
+var userPoints = document.querySelector("#user-points");   // creating a var for the timer
+var userPoints = 0;  // This will keep track of user points
+var scoreList = document.querySelector("#score-List"); //var to make the unorder list
 
 
 var hideQuizEl1 = document.querySelector(".hide-quiz-1");  // create a var to hide quiz content 1
@@ -28,6 +31,13 @@ var wrongAnswer4 = document.querySelector(".wrong-answer-4c");   // This is the 
 var wrongAnswer5 = document.querySelector(".wrong-answer-2d");   // This is the class for wrong answers
 var wrongAnswer6 = document.querySelector(".wrong-answer-3e");   // This is the class for wrong answers
 var wrongAnswer7 = document.querySelector(".wrong-answer-4f");   // This is the class for wrong answers
+
+// Create unordered list items for scoring purpose
+// This is not currrently working
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
 
 //var uncorrectAnswer = document.getElementById("#uncorrect-answer")  // This is to output " Wrong answer"
 //uncorrectAnswer.textContent = "Wrong answer!!"
@@ -69,18 +79,21 @@ wrongAnswer2.addEventListener("click", function(){
     hideQuizEl1.remove();
     console.log("Wrong Answer");
     displayQuiz2();
+    secondsLeft -= 10; //This will decrement the timer by 10 sec
 })
 
 wrongAnswer3.addEventListener("click", function(){
     hideQuizEl1.remove();
     console.log("Wrong Answer");
     displayQuiz2();
+    secondsLeft -= 10; //This will decrement the timer by 10 sec
 })
 
 wrongAnswer4.addEventListener("click", function(){
     hideQuizEl1.remove();
     console.log("Wrong Answer");
     displayQuiz2();
+    secondsLeft -=10; //This will decrement the timer by 10 sec   
 })
 
 // wrongAnswer5.addEventListener("click", function(){
@@ -106,12 +119,23 @@ rightAnswer1.addEventListener("click", function(){
     hideQuizEl1.remove();
     console.log("Right Answer");
     displayQuiz2();
+    secondsLeft +=10; //This will increment the timer by 10 sec
+    if (true) {   
+        userPoints += 5;
+        console.log(userPoints)
+    }
 })
 
 rightAnswer2.addEventListener("click", function(){
     //console.log("Right answer")
     hideQuizEl2.remove();
     console.log("Right Answer");
+    secondsLeft +=10; //This will increment the timer by 10 sec
+    if (true) {   
+        userPoints += 5;
+        console.log(userPoints)
+    }
+    userPoints.textContent = ('try')
     removeContentEnd .style.display = "block";
 })
 
@@ -127,6 +151,13 @@ function UserAnswer(){
     // }
 } */
 
+// function elseAnswer(){
+//     wrongAnswer.addEventListener("click", function(){
+//         alert('Wrong answer')
+    
+//     })
+// }
+
 
 //function to remove the content right after the button is clicked
 // I can simplify this by deleting the function and just call this "removeContentEl.remove();" 
@@ -134,21 +165,22 @@ function removeContent(){
     removeContentEl.remove();
 }
 
-function addContent(){
-    
-}
 
+//This is not working
+
+// submitEl.addEventListener('click', function{
+//     userPoints = 
+//   scoreList.append(li1);
+//    console.log(li1)
+// })
 
 startEl.addEventListener("click", function(){        //Listening for the click of the button
     setTimer();
     //console.log("test")
 })
 
-function elseAnswer(){
-    wrongAnswer.addEventListener("click", function(){
-        alert('Wrong answer')
-    
-    })
-}
+
+
+
 
 
